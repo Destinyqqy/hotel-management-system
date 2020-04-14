1168e60e30637f6d07b530863dad7f86913631a4
qiuyan qian <772142643@qq.com>
2020-04-15 01:00:17 +0800

    新增CurrUser类

---
+++ CheckIn/CurrUser.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import cv2
+from CheckIn import models
+
+class CurrUser(object):
+    '''当前客户类'''
+    def __init__(self,id=None,sex=None,name=None,agreeprivacy=False,face=None):
+        self.id=id
+        self.sex=sex
+        self.name=name
+        self.agreeprivacy=agreeprivacy
+        self.face=face
+        self.isfirst=False if models.past_customer.objects.get(pcid=id) else True
+        self.isbooked=True if models.customer.objects.get(cid=id) else False
+
+    def facesetter(self,facePath):
+        self.face=cv2.imread(facePath)
+
+        
