d365558cec14c3dd3456c0443a4708d95f32d7a7
Sharon Qin <806334626@qq.com>
2020-04-18 15:10:58 +0800

    修改personinfo界面

--- hotelmanagement/settings.py
+++ hotelmanagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'px7@_9(6@*1m%q&=j2khkhmk()b7(m7%qlm-utv5_e)s2&8*x4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
